c2da2f4b473dfdc0b4a87a96799d9711e84eca7b
Aman Bhimani <amanb014@gmail.com>
2024-12-05 21:56:13 -0500

    fix: day 5 clean

--- 2024/5.rb
+++ 2024/5.rb
@@ -4,13 +4,9 @@ class Day5 < DayRunner
   def run(sample: false)
     @input = file_input(day: 5, sample: sample)
     rules, orders = @input.slice_before("").to_a
-    rules.pop
-    orders.shift
+    rules.pop; orders.shift
 
-    @before_map = rule_map(rules)
-
-    sum = 0
-
+    rule_map = rule_map(rules)
     rule_breaks = orders.map do |order|
       passed = []
       order = order.split(",").map { |num| num.to_i }
@@ -19,9 +15,7 @@ class Day5 < DayRunner
 
       order.each do |num|
         break if broken
-        # get intersection of passed and before_map[num]
-        # if the intersection is not empty, then we have a rule break
-        if (passed & (@before_map[num] || [])).length > 0
+        if (passed & (rule_map[num] || [])).length > 0 # intersection = broken rules
           broken = true
           break;
         end
@@ -30,20 +24,14 @@ class Day5 < DayRunner
       end
 
       next if broken
-
-      # get the middle number in order
-
       order[order.length / 2]
     end
-    p rule_breaks.compact.inject(0) { |sum, x| sum + x }
+    p "Part 1: #{rule_breaks.compact.inject(0) { |sum, x| sum + x }}"
   end
 
   def rule_map(rules)
     # key = number that we are on
     # value = the number that should come AFTER the key
-    # e.g. 1 => [2] means that 1 should come before 2
-    # if we are on the "key", then we should see if any of the numbers in it's value array have already been passed
-    # if any of the numbers we have passed exist in the key's array, then we are breaking a rule.
     before_map = {}
 
     rules.each do |rule|
@@ -54,7 +42,6 @@ class Day5 < DayRunner
 
     before_map
   end
-
 end
 
 Day5.new.run
