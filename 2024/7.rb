c7ca883c0282432956eabd22b834e367ceb01c69
Aman Bhimani <amanb014@gmail.com>
2024-12-07 16:04:54 -0500

    day 7.2

--- 2024/7.rb
+++ 2024/7.rb
@@ -1,20 +1,34 @@
 require_relative './day_runner'
 
 class Day7 < DayRunner
-  def part_1(sample: false)
+  def run(sample: false)
     @input = file_input(day: 7, sample: sample)
 
     all_matched = []
+    part2_matched = []
+    unmatched_combinations = []
 
     @input.each do |line|
       result, numbers = line.split(':')
       numbers = numbers.split(" ").each(&:strip).map(&:to_i)
       result = result.to_i
 
-      all_matched << result if process_combinations(result.to_i, numbers)
+      matched = process_combinations(result, numbers)
+
+      all_matched << result if matched
+      unmatched_combinations << [result, numbers] unless matched
     end
 
-    p "Part 1: #{all_matched.sum}"
+    unmatched_combinations.each do |unmatched|
+      result, numbers = unmatched
+      matched = process_combinations_2(result, numbers)
+
+      part2_matched << result if matched
+    end
+
+    part_1_sum = all_matched.sum
+    p "Part 1: #{part_1_sum}"
+    p "Part 2: #{part_1_sum + part2_matched.sum}"
   end
 
   def process_combinations(result, numbers)
@@ -34,6 +48,27 @@ class Day7 < DayRunner
     false
   end
 
+  def process_combinations_2(result, numbers)
+    num_combinations = numbers.length - 2
+    combinations = ['*', '+', '||'].product(*([['*', '+', '||' ]] * num_combinations))
+
+    combinations.each do |combination|
+      answer = numbers[0]
+      numbers.each_with_index do |num, index|
+        next if index == 0
+
+        if combination[index - 1] == '||'
+          answer = (answer.to_s + num.to_s).to_i
+        else
+          answer = answer.send(combination[index - 1], num)
+        end
+      end
+
+      return true if  answer == result
+    end
+
+    false
+  end
 end
 
-Day7.new.part_1
+Day7.new.run
